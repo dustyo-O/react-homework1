6e5d1959042fe81ed7953845527f49d0cb4241d1
dusty <dusty@yandex-team.ru>
2021-01-18 02:11:43 +0400

    inital commit

---
+++ src/ShopItemClass/ShopItemClass.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class ShopItemClass extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.item = props.item;
+  }
+
+  render() {
+    const item = this.item;
+
+    return (
+      <div className="ShopItemClass-MainContent">
+        <h2>{item.brand}</h2>
+        <h1>{item.title}</h1>
+        <h3>{item.description}</h3>
+        <div className="ShopItemClass-Description">
+          {item.descriptionFull}
+        </div>
+        <div className="ShopItemClass-HighlightWindow_mobile"><div className="ShopItemClass-HighlightOverlay"></div></div>
+        <div className="ShopItemClass-Divider"></div>
+        <div className="ShopItemClass-PurchaseInfo">
+          <div className="ShopItemClass-Price">{item.currency}{item.price}</div>
+          <button>Добавить в корзину</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+ShopItemClass.propTypes = {
+  brand: PropTypes.string,
+  title: PropTypes.string,
+  description: PropTypes.string,
+  descriptionFull: PropTypes.string,
+  currency: PropTypes.symbol,
+  price: PropTypes.number
+};
---
+++ src/ShopItemFunc/ShopItemFunc.jsx
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types';
+
+export default function ShopItemFunc(props) {
+  const { item } = props;
+
+  return (
+    <div className="ShopItemFunc-MainContent">
+      <h2>{item.brand}</h2>
+      <h1>{item.title}</h1>
+      <h3>{item.description}</h3>
+      <div className="ShopItemFunc-Description">
+        {item.descriptionFull}
+      </div>
+      <div className="ShopItemFunc-HighlightWindow_mobile"><div className="ShopItemFunc-HighlightOverlay"></div></div>
+      <div className="ShopItemFunc-Divider"></div>
+      <div className="ShopItemFunc-PurchaseInfo">
+        <div className="ShopItemFunc-Price">{item.currency}{item.price}</div>
+        <button>Добавить в корзину</button>
+      </div>
+    </div>
+  );
+}
+
+ShopItemFunc.propTypes = {
+  brand: PropTypes.string,
+  title: PropTypes.string,
+  description: PropTypes.string,
+  descriptionFull: PropTypes.string,
+  currency: PropTypes.symbol,
+  price: PropTypes.number
+};
